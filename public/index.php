78ccb99c63d946e86634e65ee22ece8f8051fe00
Eriks <eriks.ercums@gmail.com>
2024-04-19 14:39:26 +0300

    Saglabasana

--- public/index.php
+++ public/index.php
@@ -4,33 +4,17 @@ require __DIR__ . '/../src/FuelReceiptDTO.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $receipt = new \App\FuelReceiptDTO(
-        licencePlate: $_POST["license_plate"],
-        dateTime: $_POST["date_time"],
-        petrolStation: $_POST["petrol_station"],
-        refueled: $_POST["refueled"],
-        total: $_POST["total"],
-        currency: $_POST["currency"],
-        fuelPrice: $_POST["fuel_price"],
-        odometer: $_POST["odometer"]
+        licencePlate: $_POST['license_plate'],
+        dateTime: $_POST['date_time'],
+        odometer: $_POST['odometer'],
+        petrolStation: $_POST['petrol_station'],
+        fuelType: $_POST['fuel_type'],
+        refueled: $_POST['refueled'],
+        total: $_POST['total'],
+        currency: $_POST['currency'],
     );
 
-    try {
-        $pdo = new PDO("mysql:host=mysql;dbname=fuel;charset=utf8mb4", 'root', 'root', [
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ]);
-    } catch (PDOException $e) {
-        throw new PDOException($e->getMessage(), (int)$e->getCode());
-    }
-
-    $sql = <<<MySQL
-        INSERT INTO fuel_receipts (license_plate, date_time, odometer, petrol_station, fuel_type, refueled, total, currency, fuel_price)
-        VALUES (:licencePlate, :dateTime, :odometer, :petrolStation, :fuelType, :refueled, :total, :currency, :fuelPrice)
-        MySQL;
-
-
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute($receipt->toArray());
-
+    var_dump($receipt->toArray());
 }
 
 ?>
