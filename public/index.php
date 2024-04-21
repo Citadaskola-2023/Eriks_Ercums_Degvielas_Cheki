e5640af530a203c4f5806734005d706d1dd9fc18
EriksErcums <eriks.ercums@gmail.com>
2024-04-21 17:44:58 +0300

    Update

--- public/index.php
+++ public/index.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fuel Receipt Form</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <h1>Fuel Receipt Form</h1>
--- src/FuelReceiptInsert.php
+++ src/FuelReceiptInsert.php
@@ -1,25 +1,23 @@
 <?php
 
 namespace App;
-
 use PDO;
+
 class FuelReceiptInsert
 {
-    private \PDO $pdo;
-    private function connectDB() : \PDO{
-        $conn = new PDO('localhost', 'root', 'root', 'myapp');
-        if($conn->connect_error){
-            die('Connection to database failed : ' . $conn->connect_error);
-        }
-        else{
-            return $conn;
-        }
+    private function connectDB() : PDO{
+        $conn = new PDO(
+            dsn: 'mysql:host=localhost;dbname=myapp',
+            username: 'root',
+            password: 'root');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
     }
     public function uploadFuelReceipt(array $data) : void{
         $conn = $this->connectDB();
-        $conn->prepare("INSERT INTO Form(license_plate, date_time,
+        $stmt = $conn->prepare("INSERT INTO Form(license_plate, date_time,
          petrol_station, fuel_type, refueled, total, currency, fuel_price, odometer)
-         VALUES(" . impolde(",", $data) . ")")->execute();
-        $conn->close();
+         VALUES(:license_plate, :date_time, :petrol_station, :fuel_type, :refueled, :total, :currency, :fuel_price, :odometer)");
+        $stmt->execute($data);
     }
 }
